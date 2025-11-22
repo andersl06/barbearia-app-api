83dcf912ea6f259d09ec319a02d59e242cb6a734
Anderson da Silva Linhares <anderson.linhares@supernova.local>
2025-11-22 12:52:35 -0300

    Teste

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import authRoutes from "./authRoutes.js";
-import barbershopsRoutes from "./barbershopsRoutes.js";
 import serviceRoutes from "./serviceRoutes.js";
 import scheduleRoutes from "./scheduleRoutes.js";
 import healthRoutes from "./healthRoutes.js";
@@ -9,7 +8,6 @@ import healthRoutes from "./healthRoutes.js";
 const router = Router();
 
 router.use("/auth", authRoutes);
-router.use("/barbershops", barbershopsRoutes);
 router.use("/services", serviceRoutes);
 router.use("/schedules", scheduleRoutes);
 router.use("/health", healthRoutes);
